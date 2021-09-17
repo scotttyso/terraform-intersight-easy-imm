5ef1b2f418a5a338138e96d1ed2ea8d14dbbeec7
scotttyso <tyscott@cisco.com>
2021-09-16 22:02:15 -0400

    testing local outputs

--- modules/policies/output.tf
+++ modules/policies/output.tf
@@ -1,3 +1,16 @@
 output "merged_profile_policies" {
   value = local.merged_profile_policies
 }
+
+output "data_ucs_server_profiles" {
+  value = data.terraform_remote_state.ucs_server_profiles
+}
+output "ucs_server_profiles" {
+  value = local.ucs_server_profiles
+}
+output "ucs_server_moids" {
+  value = local.ucs_server_moids
+}
+output "ucs_server_policies" {
+  value = local.ucs_server_policies
+}
